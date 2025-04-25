d78657f977032f408e181e37ca614e29e9e59fe7
Conor Blount <coblount4954@student.pps.net>
2025-04-25 12:51:40 -0700

    amination

---
+++ loadinganim.py
@@ -0,0 +1,15 @@
+from time import *
+
+input("\nEnter anything to start the animation: ")
+
+print("\n")
+
+while True:
+    print("|", end="\r")
+    sleep(0.075)
+    print("/", end="\r")
+    sleep(0.075)
+    print("-", end="\r")
+    sleep(0.075)
+    print("\\", end="\r")
+    sleep(0.075)
